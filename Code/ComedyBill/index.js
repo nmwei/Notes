8916ea85e9a9a531df12b5bb71845f38762229d3
nmwei <254060001@qq.com>
2021-07-25 21:35:50 +0800

    初始化代码

---
+++ Code/ComedyBill/index.js
@@ -0,0 +1,52 @@
+var invoices = require('./invoices');
+var plays = require('./plays');
+
+//账单
+function statement (invoice, plays) {
+    let totalAmount = 0; //总金额
+    let volumeCredits = 0; //总积分
+    let result = `Statement for ${invoice.customer}\n`;
+    const format = new Intl.NumberFormat(
+        "en-US",
+        { style: "currency", currency: "USD", minimumFractionDigits: 2 }
+     ).format; //格式化数字
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playID];
+        let thisAmount = 0;
+        switch (play.type) {
+            case "tragedy": //悲剧
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy": //喜剧
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+        // add volume credits
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        // print line for this order
+        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        totalAmount += thisAmount;
+    }
+    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
+    return result;
+}
+
+console.log(statement(invoices[0], plays));
+// Statement for BigCo
+// Hamlet: $650.00 (55 seats)
+// As You Like It: $580.00 (35 seats)
+// Othello: $500.00 (40 seats)
+// Amount owed is $1,730.00
+// You earned 47 credits
