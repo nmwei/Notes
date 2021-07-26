f28400fbf11abef5ee301db84a7222869509448c
nmwei <254060001@qq.com>
2021-07-26 17:46:36 +0800

    拆成两个文件

--- Code/ComedyBill/index.js
+++ Code/ComedyBill/index.js
@@ -1,26 +1,10 @@
 var invoices = require('./invoices');
 var plays = require('./plays');
+var createStatementData = require('./createStatementData')
 
 //账单
 function statement (invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
-}
-
-function createStatementData(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;
-}
-
-function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
 }
 
 function renderPlainText(data, plays) {
@@ -39,46 +23,6 @@ function usd(aNumber) { //格式化数字
         { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber/100);
 }
 
-function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-}
-
-function volumeCreditsFor(aPerformance) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) volumeCredits += Math.floor(aPerformance.audience / 5);
-    return volumeCredits;
-}
-
-function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-}
-
-function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-}
-
-function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return result;
-}
 
 console.log(statement(invoices[0], plays));
 // Statement for BigCo
