1129e88544846daa2c8eb3a2f10897cdd168fe95
nmwei <254060001@qq.com>
2021-07-26 11:40:44 +0800

    提炼函数 （106）

--- Code/ComedyBill/index.js
+++ Code/ComedyBill/index.js
@@ -3,9 +3,13 @@ var plays = require('./plays');
 
 //账单
 function statement (invoice, plays) {
+    const statementData = {};
+    return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
-        // print line for this order
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
     result += `Amount owed is ${usd(totalAmount(invoice))}\n`;
