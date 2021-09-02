d87cccd4101193f760e20731f20637abc18fecc5
nmwei <254060001@qq.com>
2021-09-02 21:52:35 +0800

    代码提交

--- Code/SimpleApp/SimpleApp/ViewController.m
+++ Code/SimpleApp/SimpleApp/ViewController.m
@@ -7,6 +7,35 @@
 
 #import "ViewController.h"
 
+@interface TestView : UIView
+@end
+
+@implementation TestView
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+    
+    }
+    return self;
+}
+
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview{
+    [super willMoveToSuperview:newSuperview];
+}
+- (void)didMoveToSuperview{
+    [super didMoveToSuperview];
+    
+}
+- (void)willMoveToWindow:(nullable UIWindow *)newWindow{
+    [super willMoveToWindow:newWindow];
+}
+- (void)didMoveToWindow{
+    [super didMoveToWindow];
+}
+@end
+
+
+
 @interface ViewController ()
 
 @end
@@ -16,12 +45,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self.view addSubview:({
-        UILabel *label = [[UILabel alloc] init];
-        label.text = @"hello world";
-        [label sizeToFit];
-        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
-        label;
-    })];
+//    [self.view addSubview:({
+//        UILabel *label = [[UILabel alloc] init];
+//        label.text = @"hello world";
+//        [label sizeToFit];
+//        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+//        label;
+//    })];
+    
+    TestView *view = [[TestView alloc] init];
+    view.backgroundColor = [UIColor redColor];
+    view.frame = CGRectMake(100, 100, 100, 100);
+    [self.view addSubview:view];
+    
+
+    
+    
+    
 }
 @end
